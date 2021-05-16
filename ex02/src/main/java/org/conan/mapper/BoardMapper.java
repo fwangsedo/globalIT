a623ee2725d71764344faf001b3f9f97cc1e7b4b
baekhw0729 <baekhw0729@naver.com>
2021-05-16 16:49:31 +0900

    0516test

---
+++ ex02/src/main/java/org/conan/mapper/BoardMapper.java
@@ -0,0 +1,19 @@
+package org.conan.mapper;
+
+import java.util.List;
+
+//import org.apache.ibatis.annotations.Select;
+import org.conan.domain.BoardVO;
+import org.conan.domain.Criteria;
+
+public interface BoardMapper {
+	//@Select("select * from tbl_board where bno > 0") <-이런식으로 안할거임
+	public List<BoardVO> getList();
+	public void insert(BoardVO board);
+	public void insertSelectKey(BoardVO board);
+	public BoardVO read(Long bno);
+	public int delete(Long bno);
+	public int update(BoardVO board);
+	public List<BoardVO> getListWithPaging(Criteria cri);
+	public int getTotalCount(Criteria cri);
+}
